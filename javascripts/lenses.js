a18db31ac7f4f617f4fdd2e2150874bc0a5f65e6
Jeff Zych <jlzych@gmail.com>
2012-05-27 18:35:55 -0700

    building static assets

--- javascripts/lenses.js
+++ javascripts/lenses.js
@@ -1,5 +1,5 @@
 (function() {
 
-  window.lenses = [];
+  window.lenses = ["# Lens #1: The Lens of Essential Experience\nTo use this lens, you stop thinking about your game and start thinking about the experience of the player. Ask yourself these questions:\n\n- What experience do I want the player to have?\n- What is essential to that experience?\n- How can my game capture that essence?\n\nIf there is a big difference between the experience you want to create and the one you are actually creating, your game needs to change: You need to clearly state the essential experience you desire, and find as many ways as possible to instill this essence into your game."];
 
 }).call(this);
